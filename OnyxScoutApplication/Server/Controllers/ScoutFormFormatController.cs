355cdb95b35a82c0abe7d69d824dda903aef34f5
nimrod46 <nimrodma99@gmail.com>
2020-07-13 18:15:26 +0300

    Added ScoutFormFormatController

---
+++ OnyxScoutApplication/Server/Controllers/ScoutFormFormatController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+using OnyxScoutApplication.Server.Data.Presistance.UnitsOfWork.interfaces;
+using OnyxScoutApplication.Shared.Models;
+using static OnyxScoutApplication.Server.Data.Extensions.Result;
+
+namespace OnyxScoutApplication.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ScoutFormFormatController : Controller
+    {
+        private readonly IScoutFormFormatUnitOfWork unitOfWork;
+
+        public ScoutFormFormatController(IScoutFormFormatUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ScoutFormForamt>>> Get()
+        {
+            return await unitOfWork.ScoutFormFormats.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ScoutFormForamt>> Get(int id)
+        {
+            return await unitOfWork.ScoutFormFormats.GetWithFields(id);
+        }
+
+        [HttpGet("ByYear/{year}")]
+        public async Task<ActionResult<ScoutFormForamt>> GetByYear(int year)
+        {
+            return await unitOfWork.ScoutFormFormats.GetWithFieldsByYear(year);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateScoutFormFormat(int id, [FromBody] ScoutFormForamt scoutFormForamtModel)
+        {
+            var response = await unitOfWork.Update(id, scoutFormForamtModel);
+            await unitOfWork.Complete();
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateScoutFormFormat([FromBody] ScoutFormForamt scoutFormForamtModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await unitOfWork.ScoutFormFormats.Add(scoutFormForamtModel);
+                await unitOfWork.Complete();
+                return response;
+            }
+            return ResultCode(System.Net.HttpStatusCode.BadRequest, "Invalid inputs!");
+        }
+    }
+}
--- OnyxScoutApplication/Server/Data/Presistance/Repositories/ScoutFormFormatRepository.cs
+++ OnyxScoutApplication/Server/Data/Presistance/Repositories/ScoutFormFormatRepository.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static OnyxScoutApplication.Server.Data.Extensions.Result;
+
 namespace OnyxScoutApplication.Server.Data.Presistance.Repositories
 {
     public class ScoutFormFormatRepository : Repository<ScoutFormForamt>, IScoutFormFormatRepository
