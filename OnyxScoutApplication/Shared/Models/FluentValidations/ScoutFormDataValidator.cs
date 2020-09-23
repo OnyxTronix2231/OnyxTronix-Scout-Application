01cac5883e8fd68180b0158663ff82f27e1a2a0b
nimrod46 <nimrodma99@gmail.com>
2020-09-23 20:36:45 +0300

    Finished ScoutFrom create page with validations

---
+++ OnyxScoutApplication/Shared/Models/FluentValidations/ScoutFormDataValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnyxScoutApplication.Shared.Models.FluentValidations
+{
+    public class ScoutFormDataValidator : AbstractValidator<ScoutFormDataDto>
+    {
+        public ScoutFormDataValidator()
+        {
+            RuleFor(x => x.NumricValue).Must((model, s) => BeNumberBetween(model, s)).
+                WithMessage((model, s) => "Value must be a number between " + model.Field.MinValue + " and " + model.Field.MaxValue).When(c => c.Field.FieldType == FieldType.Numeric);
+            RuleFor(x => x.NumricValue).NotEmpty().WithMessage((model, s) => "Field required").When(m => m.Field.Required && m.Field.FieldType == FieldType.Numeric);
+            RuleFor(x => x.StringValue).NotEmpty().WithMessage((model, s) => "Field required").When(m => m.Field.Required && m.Field.FieldType == FieldType.TextField);
+        }
+
+        private bool BeNumberBetween(ScoutFormDataDto model, int? value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            int v = (int)value;
+            return v >= model.Field.MinValue && v <= model.Field.MaxValue;
+        }
+    }
+}
---
+++ OnyxScoutApplication/Shared/Models/FluentValidations/ScoutFormValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnyxScoutApplication.Shared.Models.FluentValidations
+{
+    public class ScoutFormForamtValidator : AbstractValidator<ScoutFormFormatDto>
+    {
+        public ScoutFormForamtValidator()
+        {
+            RuleFor(x => x.Year).GreaterThanOrEqualTo(2000).LessThanOrEqualTo(2099);
+            RuleForEach(x => x.AutonomousFields).SetValidator(new FieldValidator());
+            RuleForEach(x => x.TeleoperatedFields).SetValidator(new FieldValidator());
+            RuleForEach(x => x.EndGameFields).SetValidator(new FieldValidator());
+        }
+    }
+}
