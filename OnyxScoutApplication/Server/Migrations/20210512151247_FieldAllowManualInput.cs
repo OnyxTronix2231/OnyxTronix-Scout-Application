c1d5c4076a9c0bc00ff5713184ea2817e3c01404
nimrod46 <nimrodma99@gmail.com>
2021-05-12 20:41:38 +0300

    Added Manual Input in timer

---
+++ OnyxScoutApplication/Server/Migrations/20210512151247_FieldAllowManualInput.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnyxScoutApplication.Server.Migrations
+{
+    public partial class FieldAllowManualInput : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowManualInput",
+                table: "Field",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowManualInput",
+                table: "Field");
+        }
+    }
+}
