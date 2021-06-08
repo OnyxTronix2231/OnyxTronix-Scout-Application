b4a3ed75dbcf7b22e47cd4c901a8cca2e923a67c
nimrod46 <nimrodma99@gmail.com>
2021-06-08 19:28:45 +0300

    Update ExtensionsMethods.cs

--- OnyxScoutApplication/Server/Data/Extensions/ExtensionsMethods.cs
+++ OnyxScoutApplication/Server/Data/Extensions/ExtensionsMethods.cs
@@ -18,39 +18,33 @@ namespace OnyxScoutApplication.Server.Data.Extensions
                 .Include(i => i.Matches).ThenInclude(i => i.Alliances).ThenInclude(i => i.Red)
                 .ThenInclude(i => i.Teams);
         }
-        
+
         public static IQueryable<Form> WithAllData(this IQueryable<Form> queryable)
         {
             return queryable.Include(i => i.FormDataInStages).ThenInclude(i => i.FormData).ThenInclude(i => i.Field)
                 .ThenInclude(i => i.Options)
-
                 .Include(i => i.FormDataInStages).ThenInclude(i => i.FormData)
                 .ThenInclude(i => i.CascadeData).ThenInclude(i => i.Field)
                 .ThenInclude(i => i.Options)
-
                 .Include(i => i.FormDataInStages).ThenInclude(i => i.FormData)
                 .ThenInclude(i => i.CascadeData).ThenInclude(i => i.CascadeData).ThenInclude(i => i.Field)
                 .ThenInclude(i => i.Options)
-
                 .Include(i => i.FormDataInStages).ThenInclude(i => i.FormData)
                 .ThenInclude(i => i.CascadeData).ThenInclude(i => i.CascadeData).ThenInclude(i => i.CascadeData)
                 .ThenInclude(i => i.Field).ThenInclude(i => i.Options).AsSplitQuery();
         }
-        
+
         public static IQueryable<ScoutFormFormat>
             WithAllFields(this IQueryable<ScoutFormFormat> queryable)
         {
             return queryable.AsSplitQuery().Include(i => i.FieldsInStages)
                 .ThenInclude(f => f.Fields).ThenInclude(i => i.Options)
-
                 .Include(i => i.FieldsInStages)
                 .ThenInclude(f => f.Fields)
                 .ThenInclude(i => i.CascadeFields).ThenInclude(i => i.Options)
-
                 .Include(i => i.FieldsInStages)
                 .ThenInclude(f => f.Fields)
                 .ThenInclude(i => i.CascadeFields).ThenInclude(i => i.CascadeFields).ThenInclude(i => i.Options)
-
                 .Include(i => i.FieldsInStages)
                 .ThenInclude(f => f.Fields)
                 .ThenInclude(i => i.CascadeFields).ThenInclude(i => i.CascadeFields)
