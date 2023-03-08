6d1391e6ad98b16c908b6bfb76d8f239d5723ce3
nimrod46 <nimrodma99@gmail.com>
2023-03-08 19:32:12 +0200

    Revert "Added GC call for testing"
    This reverts commit 91a272752670fdab8a0ba3ddefc08a76c6cf3b63.

--- OnyxScoutApplication/Server/Controllers/ScoutFormController.cs
+++ OnyxScoutApplication/Server/Controllers/ScoutFormController.cs
@@ -123,8 +123,6 @@ namespace OnyxScoutApplication.Server.Controllers
         public async Task<ActionResult<IEnumerable<FormDto>>> GetAllByEventWithData(string eventKey,
             ScoutFormType scoutFormType = ScoutFormType.MainGame)
         {
-            System.GC.Collect();
-            System.GC.WaitForPendingFinalizers();
             return await unitOfWork.ScoutForms.GetAllByEventWithData(eventKey, scoutFormType);
         }
 
