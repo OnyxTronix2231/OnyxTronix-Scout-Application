91e8664af70a584b78505f224add7ac67f629fb5
nimrod46 <nimrodma99@gmail.com>
2023-03-18 13:39:33 +0200

    Fixed teams notes

--- OnyxScoutApplication/Server/Controllers/EventAnalyticsController.cs
+++ OnyxScoutApplication/Server/Controllers/EventAnalyticsController.cs
@@ -33,47 +33,59 @@ public class EventAnalyticsController : Controller
         this.scoutFormFormatUnitOfWork = scoutFormFormatUnitOfWork;
         this.blueAllianceService = blueAllianceService;
     }
-    
+
     [HttpPost("GetEventAnalytics/{year:int}/{eventKey}")]
-    public async Task<ActionResult<AnalyticsResult>> GetEventAnalytics(int year, string eventKey, AnalyticsSettings analyticsSettings)
+    public async Task<ActionResult<AnalyticsResult>> GetEventAnalytics(int year, string eventKey,
+        AnalyticsSettings analyticsSettings)
     {
-        var scoutFormsRes = await scoutFormUnitOfWork.ScoutForms.GetAllByEventWithData(eventKey, ScoutFormType.MainGame);
-        var scoutForms = scoutFormsRes.Value!
-            .Where(f => f.DateTime >= analyticsSettings.StartDate && f.DateTime <= analyticsSettings.EndDate).ToList();
+        var scoutFormsRes =
+            await scoutFormUnitOfWork.ScoutForms.GetAllByEventWithData(eventKey, ScoutFormType.MainGame);
 
         if (scoutFormsRes.Result is not null)
         {
             return scoutFormsRes.Result;
         }
-        var scoutFormFormat = await scoutFormFormatUnitOfWork.ScoutFormFormats.GetWithFieldsByYear(year, ScoutFormType.MainGame);
+
+        var scoutForms = scoutFormsRes.Value!
+            .Where(f => f.DateTime >= analyticsSettings.StartDate && f.DateTime <= analyticsSettings.EndDate).ToList();
+
+
+        var scoutFormFormat =
+            await scoutFormFormatUnitOfWork.ScoutFormFormats.GetWithFieldsByYear(year, ScoutFormType.MainGame);
         if (scoutFormFormat.Result is not null)
         {
             return scoutFormFormat.Result;
         }
+
         var teams = await blueAllianceService.GetTeamsByEvent(eventKey);
-        TeamsAnalyzer analyzer = new TeamsAnalyzer(teams, scoutForms, scoutFormFormat.Value, analyticsSettings.EventAnalyticSettingsDto);
+        TeamsAnalyzer analyzer = new TeamsAnalyzer(teams, scoutForms, scoutFormFormat.Value,
+            analyticsSettings.EventAnalyticSettingsDto);
         return Ok(analyzer.Calc());
     }
-    
+
     [HttpPost("GetTeamEventAnalytics/{year:int}/{eventKey}/{teamNumber:int}")]
-    public async Task<ActionResult<AnalyticsResult>> GetTeamEventAnalytics(int year, string eventKey, int teamNumber, AnalyticsSettings analyticsSettings)
+    public async Task<ActionResult<AnalyticsResult>> GetTeamEventAnalytics(int year, string eventKey, int teamNumber,
+        AnalyticsSettings analyticsSettings)
     {
         Dictionary<string, List<TeamFieldAverage>> calculatedScoutDataByStages =
             new Dictionary<string, List<TeamFieldAverage>>();
-        
+
         Dictionary<string, List<TeamFieldAverage>> calculatedScoutDataByStagesPit =
             new Dictionary<string, List<TeamFieldAverage>>();
-        
-        
-        var scoutFormsRes = await scoutFormUnitOfWork.ScoutForms.GetAllByTeamWithData(teamNumber, eventKey, ScoutFormType.MainGame);
+
+
+        var scoutFormsRes =
+            await scoutFormUnitOfWork.ScoutForms.GetAllByTeamWithData(teamNumber, eventKey, ScoutFormType.MainGame);
         if (scoutFormsRes.Result is not null)
         {
             return scoutFormsRes.Result;
         }
+
         var scoutForms = scoutFormsRes.Value!
             .Where(f => f.DateTime >= analyticsSettings.StartDate && f.DateTime <= analyticsSettings.EndDate).ToList();
 
-        var scoutFormFormat = await scoutFormFormatUnitOfWork.ScoutFormFormats.GetWithFieldsByYear(year, ScoutFormType.MainGame);
+        var scoutFormFormat =
+            await scoutFormFormatUnitOfWork.ScoutFormFormats.GetWithFieldsByYear(year, ScoutFormType.MainGame);
         if (scoutFormFormat.Result is not null)
         {
             return scoutFormFormat.Result;
@@ -84,36 +96,57 @@ public class EventAnalyticsController : Controller
             calculatedScoutDataByStages.Add(fieldsInStage.Name,
                 TeamDataAnalyzer.CalculateDataFor(fieldsInStage, scoutForms, _ => true));
         }
-        
-        
-        
-        var scoutFormsPitRes = await scoutFormUnitOfWork.ScoutForms.GetAllByTeamWithData(teamNumber, eventKey, ScoutFormType.Pit);
+
+
+        var scoutFormsPitRes =
+            await scoutFormUnitOfWork.ScoutForms.GetAllByTeamWithData(teamNumber, eventKey, ScoutFormType.Pit);
         if (scoutFormsPitRes.Result is not null)
         {
             return scoutFormsPitRes.Result;
         }
-        
+
         var scoutFormsPit = scoutFormsPitRes.Value!.ToList();
-        
-        var scoutFormFormatPit = await scoutFormFormatUnitOfWork.ScoutFormFormats.GetWithFieldsByYear(year, ScoutFormType.Pit);
+
+        var scoutFormFormatPit =
+            await scoutFormFormatUnitOfWork.ScoutFormFormats.GetWithFieldsByYear(year, ScoutFormType.Pit);
         if (scoutFormFormatPit.Result is not null)
         {
             return scoutFormFormatPit.Result;
         }
-        
+
         foreach (var fieldsInStage in scoutFormFormatPit.Value!.FieldsInStages)
         {
             calculatedScoutDataByStagesPit.Add(fieldsInStage.Name,
                 TeamDataAnalyzer.CalculateDataFor(fieldsInStage, scoutFormsPit, _ => true));
         }
-        
+
         return Ok(new AnalyticsTeamResult
         {
             CalculatedScoutDataByStages = calculatedScoutDataByStages,
             CalculatedScoutDataByStagesPit = calculatedScoutDataByStagesPit
         });
     }
-    
-    
 
+
+    [HttpGet("GetNotesOnTeam/{year:int}/{eventKey}/{teamNumber:int}")]
+    public async Task<ActionResult<Dictionary<string, List<Tuple<FormDataDto[], string>>>>> GetNotesOnTeam(int year,
+        string eventKey, int teamNumber)
+    {
+        var scoutFormsRes =
+            await scoutFormUnitOfWork.ScoutForms.GetAllByTeamWithData(teamNumber, eventKey, ScoutFormType.MainGame);
+
+        if (scoutFormsRes.Result is not null)
+        {
+            return scoutFormsRes.Result;
+        }
+
+        var scoutForms = scoutFormsRes.Value!;
+
+        var v = scoutForms.SelectMany(i => i.FormDataInStages.ToDictionary(ii => ii.Name, ii =>
+            new Tuple<FormDataDto[], string>(ii.FormData.Where(d => d.Field.FieldType == FieldType.TextField).ToArray(),
+                i.WriterUserName)));
+
+        var notesByStage = v.ToLookup(i => i.Key, i => i.Value).ToDictionary(i => i.Key, i => i.ToList());
+        return Ok(notesByStage);
+    }
 }
