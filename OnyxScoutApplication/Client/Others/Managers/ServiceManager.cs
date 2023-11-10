20e9a28bc94a4409bce54a00bb651d84a0e59047
nimrod46 <nimrodma99@gmail.com>
2023-11-10 23:08:21 +0200

    Added server status get method

--- OnyxScoutApplication/Client/Others/Managers/ServiceManager.cs
+++ OnyxScoutApplication/Client/Others/Managers/ServiceManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using OnyxScoutApplication.Client.Shared.Services;
+using OnyxScoutApplication.Shared.Models;
 
 namespace OnyxScoutApplication.Client.Others.Managers;
 
@@ -10,18 +11,28 @@ public class ServiceManager
     private readonly TheBlueAllianceService theBlueAllianceService;
     private readonly ScoutFormFormatService formatService;
     private readonly ScoutFormService formService;
+    private readonly HttpClientManager httpClientManager;
+    private readonly AppManager appManager;
 
     public ServiceManager(EventService eventService, TheBlueAllianceService theBlueAllianceService,
-        ScoutFormFormatService formatService, ScoutFormService formService)
+        ScoutFormFormatService formatService, ScoutFormService formService, HttpClientManager httpClientManager,
+        AppManager appManager)
     {
         this.eventService = eventService;
         this.theBlueAllianceService = theBlueAllianceService;
         this.formatService = formatService;
         this.formService = formService;
+        this.httpClientManager = httpClientManager;
+        this.appManager = appManager;
     }
 
     public async Task CleanAndInitAllServices()
     {
+        if (await httpClientManager.GetJson<ServerStatus>("ScoutForm/ServerStatus") is null)
+        {
+            appManager.IsOnlineMode = false;
+        }
+        
         await eventService.OnInit();
         if (await eventService.GetSelectedEvent() is null)
         {
